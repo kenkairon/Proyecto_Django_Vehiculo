e0129c3863bed036f90e0bcc932c036bdfe5435a
kenkairon <ingeinfo2006@gmail.com>
2026-09-11 18:26:48 -0300

    Agregar static

--- config/config/settings.py
+++ config/config/settings.py
@@ -122,8 +122,8 @@ LOGIN_URL = '/login/'  # Esta es la URL que se usará para redirigir si no hay s
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
