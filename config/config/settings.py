f8571e4a63b7b03f3eb775c96c58ca54be0a15ab
kenkairon <ingeinfo2006@gmail.com>
2026-09-11 18:59:46 -0300

    cambio de configuracion

--- config/config/settings.py
+++ config/config/settings.py
@@ -26,6 +26,11 @@ load_dotenv()
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG", "False") == "True"  
 
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "proyecto-django-vehiculo.onrender.com",
+]
 
 # Application definition
 
